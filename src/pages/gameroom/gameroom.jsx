1d6fe368cf5448802503f93c9fd70203eb4be8cf
hbk671104 <hbk671104@gmail.com>
2021-07-31 21:30:07 +0800

    add actionsheet

--- src/pages/gameroom/gameroom.jsx
+++ src/pages/gameroom/gameroom.jsx
@@ -1,163 +1,144 @@
-import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import { AtFab, AtDrawer, AtButton } from 'taro-ui'
-import Taro, { getCurrentInstance } from '@tarojs/taro'
-import AV from 'leancloud-storage/dist/av-live-query-weapp.js'
+import React, { Component } from "react";
+import { View, Text } from "@tarojs/components";
+import { AtFab, AtActionSheet, AtActionSheetItem } from "taro-ui";
+import Taro, { getCurrentInstance } from "@tarojs/taro";
+import AV from "leancloud-storage/dist/av-live-query-weapp.js";
 
-import Player from '../../components/player'
-import './gameroom.scss'
+import Player from "../../components/player";
+import "./gameroom.scss";
 
 export default class Gameroom extends Component {
-
   state = {
     score: null,
-    showDrawer: false
-  }
+    showActionSheet: false
+  };
 
   componentWillMount() {
     if (!AV.User.current()) {
-      const { roomId } = getCurrentInstance().router.params
+      const { roomId } = getCurrentInstance().router.params;
       Taro.redirectTo({
         url: `../login/login?roomId=${roomId}`
-      })
+      });
     }
   }
 
   componentDidMount() {
-    this.fetch()
-    // this.subscribe()
+    this.fetch();
+    this.subscribe();
   }
 
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
   componentDidShow() {
-    Taro.hideHomeButton()
+    Taro.hideHomeButton();
   }
 
-  componentDidHide() { }
+  componentDidHide() {}
 
   onShareAppMessage(options) {
-    const { roomId } = getCurrentInstance().router.params
+    const { roomId } = getCurrentInstance().router.params;
     return {
-      title: '房间已开，快来加入！',
+      title: "房间已开，快来加入！",
       path: `pages/gameroom/gameroom?roomId=${roomId}`
-    }
+    };
   }
 
-  onPlusClick = p => e => {
-    e.stopPropagation()
-  }
+  onPlusClick = p => () => {};
 
-  onMenuClick = e => {
-    e.stopPropagation()
-    this.setState({
-      showDrawer: true
-    })
-  }
-
-  onCloseRoomButtonClick = e => {
-    e.stopPropagation()
+  onCloseRoomButtonClick = () => {
     Taro.showModal({
-      title: '提示',
-      content: '确定关闭房间吗？',
+      title: "提示",
+      content: "确定关闭房间吗？",
       success: res => {
         if (res.confirm) {
-          this.closeRoom()
+          this.closeRoom();
         }
       }
-    })
-  }
+    });
+  };
 
   fetch = async () => {
-    const { roomId } = getCurrentInstance().router.params
-    const query = new AV.Query('Room').equalTo('objectId', roomId)
+    const { roomId } = getCurrentInstance().router.params;
+    const query = new AV.Query("Room").equalTo("objectId", roomId);
 
-    Taro.showNavigationBarLoading()
+    Taro.showNavigationBarLoading();
     try {
-      const { score } = (await query.get(roomId)).toJSON()
+      const { score } = (await query.get(roomId)).toJSON();
       this.setState({
         score
-      })
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     } finally {
-      Taro.hideNavigationBarLoading()
+      Taro.hideNavigationBarLoading();
     }
-  }
+  };
 
   subscribe = async () => {
-    const { roomId } = getCurrentInstance().router.params
-    const query = new AV.Query('Room').equalTo('objectId', roomId)
+    const { roomId } = getCurrentInstance().router.params;
+    const query = new AV.Query("Room").equalTo("objectId", roomId);
 
     try {
-      const liveQuery = await query.subscribe()
-      liveQuery.on('update', (room, keys) => {
-        console.log(keys)
-      })
+      const liveQuery = await query.subscribe();
+      liveQuery.on("update", (room, keys) => {
+        console.log(room.toJSON(), keys);
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }
+  };
 
   closeRoom = async () => {
-    const { roomId } = getCurrentInstance().router.params
-    const room = AV.Object.createWithoutData('Room', roomId)
-    room.set('status', 'closed')
+    const { roomId } = getCurrentInstance().router.params;
+    const room = AV.Object.createWithoutData("Room", roomId);
+    room.set("status", "closed");
 
     Taro.showLoading({
-      title: '正在关闭房间...'
-    })
+      title: "正在关闭房间..."
+    });
     try {
-      await room.save()
+      await room.save();
       Taro.redirectTo({
-        url: '../index/index'
-      })
+        url: "../index/index"
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     } finally {
-      Taro.hideLoading()
+      Taro.hideLoading();
     }
-  }
+  };
 
   render() {
-    const { score, showDrawer } = this.state
+    const { score, showActionSheet } = this.state;
     return (
-      <View className='page gameroom'>
-        {
-          !!score && Object.keys(score).map(p =>
+      <View className="page gameroom">
+        {!!score &&
+          Object.keys(score).map(p => (
             <Player
               key={p}
               id={p}
               score={score[p]}
               onPlusClick={this.onPlusClick(p)}
             />
-          )
-        }
-        <AtFab className='menu' onClick={this.onMenuClick}>
-          <Text className='at-fab__icon at-icon at-icon-menu'></Text>
+          ))}
+        <AtFab
+          className="menu"
+          onClick={() => this.setState({ showActionSheet: true })}
+        >
+          <Text className="at-fab__icon at-icon at-icon-menu"></Text>
         </AtFab>
-        <AtDrawer show={showDrawer} right>
-          <View className='page drawer'>
-            <View className='group'>
-              <View>
-                <AtButton className='create-room'>
-                  账单记录
-                </AtButton>
-              </View>
-              <View style='margin-top:12px;'>
-                <AtButton openType='share'>
-                  邀请玩家
-                </AtButton>
-              </View>
-              <View style='margin-top:12px;'>
-                <AtButton onClick={this.onCloseRoomButtonClick}>
-                  关闭房间
-                </AtButton>
-              </View>
-            </View>
-          </View>
-        </AtDrawer>
+        <AtActionSheet
+          isOpened={showActionSheet}
+          cancelText="取消"
+          onClose={() => this.setState({ showActionSheet: false })}
+        >
+          <AtActionSheetItem>邀请玩家</AtActionSheetItem>
+          <AtActionSheetItem>账单记录</AtActionSheetItem>
+          <AtActionSheetItem onClick={this.onCloseRoomButtonClick}>
+            <Text style="color:red;">关闭房间</Text>
+          </AtActionSheetItem>
+        </AtActionSheet>
       </View>
-    )
+    );
   }
 }
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,72 +1,67 @@
-import React, { Component } from 'react'
-import Taro from '@tarojs/taro'
-import { View, Image } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
-import AV from 'leancloud-storage/dist/av-live-query-weapp.js'
+import React, { Component } from "react";
+import Taro from "@tarojs/taro";
+import { View, Image } from "@tarojs/components";
+import { AtButton } from "taro-ui";
+import AV from "leancloud-storage/dist/av-live-query-weapp.js";
 
-import './index.scss'
+import "./index.scss";
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount() { }
+  componentDidMount() {}
 
-  componentDidMount() { }
-
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
   componentDidShow() {
-    Taro.hideHomeButton()
+    Taro.hideHomeButton();
   }
 
-  componentDidHide() { }
+  componentDidHide() {}
 
-  onRoomCreateClick = e => {
-    e.stopPropagation()
-    this.createRoom()
-  }
+  onRoomCreateClick = () => {
+    this.createRoom();
+  };
 
-  onRoomJoinClick = e => {
-    e.stopPropagation()
+  onRoomJoinClick = () => {
     // this.joinRoom()
-  }
+  };
 
   createRoom = async () => {
-    let room = new AV.Object('Room')
-    const user = AV.User.current()
-    room.set('owner', user)
-    room.set('score', {
-      [user.get('objectId')]: 0
-    })
+    let room = new AV.Object("Room");
+    const user = AV.User.current();
+    room.set("owner", user);
+    room.set("score", {
+      [user.get("objectId")]: 0
+    });
 
-    Taro.showLoading({ title: '正在创建房间...' })
+    Taro.showLoading({ title: "正在创建房间..." });
     try {
-      room = await room.save()
-      const roomId = room.get('objectId')
+      room = await room.save();
+      const roomId = room.get("objectId");
       Taro.redirectTo({
         url: `../gameroom/gameroom?roomId=${roomId}`
-      })
+      });
     } catch (error) {
-      console.error(error)
+      console.error(error);
     } finally {
-      Taro.hideLoading()
+      Taro.hideLoading();
     }
-  }
+  };
 
   render() {
     return (
-      <View className='page index'>
-        <View className='group'>
-          <Image className='logo' src={require('../../assets/logo.png')} />
+      <View className="page index">
+        <View className="group">
+          <Image className="logo" src={require("../../assets/logo.png")} />
         </View>
-        <View className='button-group'>
-          <AtButton className='create-room' onClick={this.onRoomCreateClick}>
+        <View className="button-group">
+          <AtButton className="create-room" onClick={this.onRoomCreateClick}>
             创建房间
           </AtButton>
-          <AtButton onClick={this.onRoomJoinClick}>
-            加入房间
-          </AtButton>
+          <AtButton onClick={this.onRoomJoinClick}>加入房间</AtButton>
         </View>
       </View>
-    )
+    );
   }
 }
